420d73518c5f8ba6a54cd81e6a732ecf985c1ec3
muskan bajpai <muskan.bajpai124@gmail.com>
2024-07-08 19:06:32 +0530

    #7 utility

--- pattern1.py
+++ pattern1.py
@@ -1,11 +1,31 @@
 from turtle import  *
+'''fillcolor('green')
+begin_fill()
+for i in range (5):
+    fd(100)
+    lt(72)
+end_fill()
+fillcolor('yellow')
+begin_fill()
+circle(50)
+end_fill()
+fillcolor("red")
+begin_fill()
+for i in range(4):
+    fd(100)
+    lt(90)
+end_fill()
+speed('slow')
+pencolor('black')
+for i in range (100):
+    fd(i*3+5)
+    lt(90)'''
 s = Screen()
 s.setup(1000,700)
-color = ['blue','purple']
+color = ['blue','red']
 pencolor('green')
 pensize(3)
-
-
+for i in range()
 mainloop()
 
 
